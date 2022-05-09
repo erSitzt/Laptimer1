5fb4e99a5d7a0ccb64e222601da5e00bcb038675
erSitzt <1625376+erSitzt@users.noreply.github.com>
2022-05-10 00:54:34 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace Laptimer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.objectListView1 = new BrightIdeasSoftware.ObjectListView();
+            this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.textBox_laptimeService = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(996, 656);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(548, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(13, 656);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(977, 264);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(13, 13);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(759, 544);
+            this.listBox3.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // objectListView1
+            // 
+            this.objectListView1.AllColumns.Add(this.olvColumn1);
+            this.objectListView1.AllColumns.Add(this.olvColumn2);
+            this.objectListView1.AllColumns.Add(this.olvColumn3);
+            this.objectListView1.CellEditUseWholeCell = false;
+            this.objectListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvColumn1,
+            this.olvColumn2,
+            this.olvColumn3});
+            this.objectListView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.objectListView1.HideSelection = false;
+            this.objectListView1.Location = new System.Drawing.Point(868, 160);
+            this.objectListView1.Name = "objectListView1";
+            this.objectListView1.Size = new System.Drawing.Size(522, 375);
+            this.objectListView1.TabIndex = 3;
+            this.objectListView1.UseCompatibleStateImageBehavior = false;
+            this.objectListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // olvColumn1
+            // 
+            this.olvColumn1.AspectName = "TagID";
+            // 
+            // olvColumn2
+            // 
+            this.olvColumn2.AspectName = "TagSeenCount";
+            this.olvColumn2.AspectToStringFormat = "{0:d}";
+            // 
+            // olvColumn3
+            // 
+            this.olvColumn3.AspectName = "TagEvent";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 1116);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1556, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // textBox_laptimeService
+            // 
+            this.textBox_laptimeService.Location = new System.Drawing.Point(141, 1084);
+            this.textBox_laptimeService.Name = "textBox_laptimeService";
+            this.textBox_laptimeService.Size = new System.Drawing.Size(326, 26);
+            this.textBox_laptimeService.TabIndex = 5;
+            this.textBox_laptimeService.Text = "http://192.168.178.20:3000";
+            this.textBox_laptimeService.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 1087);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Laptime Service";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1556, 1138);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_laptimeService);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.objectListView1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Timer timer1;
+        private BrightIdeasSoftware.ObjectListView objectListView1;
+        private BrightIdeasSoftware.OLVColumn olvColumn1;
+        private BrightIdeasSoftware.OLVColumn olvColumn2;
+        private BrightIdeasSoftware.OLVColumn olvColumn3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox_laptimeService;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using Symbol.RFID3;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Laptimer1
+{
+    public partial class Form1 : Form
+    {
+        RFIDReader rfid3;
+        Dictionary<String, Tag> tagsdict;
+        private RadioButton[] antennas = new RadioButton[10];
+
+        private UpdateRead UpdateReadHandler = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string hostname = "192.168.178.100";
+
+
+            UpdateReadHandler = new UpdateRead(myUpdateRead);
+
+            rfid3 = new RFIDReader(hostname, 5084, 2000);
+
+            tagsdict = new Dictionary<String, Tag>();
+
+
+            rfid3.Connect();
+
+
+            listBox1.Items.Add(String.Format("FirwareVersion={0}", rfid3.ReaderCapabilities.FirwareVersion));
+
+            TriggerInfo triggerInfo = new TriggerInfo();
+            triggerInfo.EnableTagEventReport = true;
+            triggerInfo.TagEventReportInfo.ReportNewTagEvent = TAG_EVENT_REPORT_TRIGGER.MODERATED;
+            triggerInfo.TagEventReportInfo.ReportTagInvisibleEvent = TAG_EVENT_REPORT_TRIGGER.MODERATED;
+            triggerInfo.TagEventReportInfo.ReportTagBackToVisibilityEvent = TAG_EVENT_REPORT_TRIGGER.MODERATED;
+            triggerInfo.TagEventReportInfo.NewTagEventModeratedTimeoutMilliseconds = 200;
+            triggerInfo.TagEventReportInfo.TagInvisibleEventModeratedTimeoutMilliseconds = 3000;
+            triggerInfo.TagEventReportInfo.TagBackToVisibilityModeratedTimeoutMilliseconds = 200;
+
+            // registering for read tag data notification
+
+            rfid3.Events.ReadNotify += new Events.ReadNotifyHandler(Events_ReadNotify);
+
+            // ReadNotify Event comes without tag data 
+
+            rfid3.Events.AttachTagDataWithReadEvent = true;
+
+            rfid3.Actions.Inventory.Perform(null, triggerInfo, null);
+            timer1.Start();
+        }
+        private delegate void Update();
+        public void addlist(string item)
+        {
+            listBox2.Invoke(new Update(() => listBox2.Items.Add(item)));
+        }
+        private delegate void UpdateRead(Events.ReadEventData eventData);
+        private async void  myUpdateRead(Events.ReadEventData eventData)
+        {
+            if (rfid3.Events.AttachTagDataWithReadEvent)
+            {
+                addlist(String.Format("TagID : {0} : {1} : {2} : {3} : {4}", eventData.TagData.TagID, eventData.TagData.AntennaID, eventData.TagData.TagEvent, eventData.TagData.tagTransitionStatus, eventData.TagData.TagEventTimeStamp));
+                Tag tmptag = new Tag(eventData.TagData.TagID, eventData.TagData.TagEventTimeStamp);
+                //tagsdict.Add(tmptag.TagId, tmptag);
+                if (eventData.TagData.OpCode == ACCESS_OPERATION_CODE.ACCESS_OPERATION_READ && eventData.TagData.OpStatus == ACCESS_OPERATION_STATUS.ACCESS_SUCCESS)
+                {
+                    addlist(String.Format("{0} : {1}", eventData.TagData.MemoryBank.ToString(), eventData.TagData.MemoryBankData));
+
+                }
+                if (eventData.TagData.TagEvent == TAG_EVENT.NEW_TAG_VISIBLE || eventData.TagData.TagEvent == TAG_EVENT.TAG_BACK_TO_VISIBILITY)
+                {
+
+                    var json = JsonConvert.SerializeObject(tmptag);
+                    var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    var url = textBox_laptimeService.Text + "/lap/" + tmptag.TagId;
+                    var client = new HttpClient();
+
+                    var response = await client.PostAsync(url, data);
+                    var result = await response.Content.ReadAsStringAsync();
+
+
+
+
+                }
+            }
+            else
+            {
+                Symbol.RFID3.TagData[] tagData = rfid3.Actions.GetReadTags(1000);
+                objectListView1.SetObjects(tagData);
+            }
+            
+
+
+        }
+        public void Events_ReadNotify(object sender, Events.ReadEventArgs readEventArgs)
+        {
+            try
+            {
+                this.Invoke(UpdateReadHandler, new object[] { readEventArgs.ReadEventData });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            /*
+            addlist(String.Format("TagID : {0} : {1} : {2} : {3} : {4}", e.ReadEventData.TagData.TagID, e.ReadEventData.TagData.AntennaID, e.ReadEventData.TagData.TagEvent, e.ReadEventData.TagData.tagTransitionStatus, e.ReadEventData.TagData.TagEventTimeStamp));
+            Tag tmptag = new Tag(e.ReadEventData.TagData.TagID);
+            //tagsdict.Add(tmptag.TagId, tmptag);
+            if (e.ReadEventData.TagData.OpCode == ACCESS_OPERATION_CODE.ACCESS_OPERATION_READ && e.ReadEventData.TagData.OpStatus == ACCESS_OPERATION_STATUS.ACCESS_SUCCESS)
+            {
+                addlist(String.Format("{0} : {1}", e.ReadEventData.TagData.MemoryBank.ToString(), e.ReadEventData.TagData.MemoryBankData));
+
+            }
+            */
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            objectListView1.BuildList();
+            listBox3.Items.Clear();
+            TagData[] seenTags = rfid3.Actions.GetReadTags(1000);
+            if (seenTags != null)
+            {
+                for (int nIndex = 0; nIndex < seenTags.Length; nIndex++)
+                {
+                    listBox3.Items.Add(String.Format("{0} : Seen : {1}", seenTags[nIndex].TagID, seenTags[nIndex].TagSeenCount));
+                }
+            }
+        }
+    }
+    class Tag
+    {
+        public Tag(string TagId, DateTime TagSeenTime)
+        {
+            this.TagId = TagId;
+            this.SeenCount = 0;
+            this.TagSeenTime = TagSeenTime;
+        }
+
+        public string TagId { get; set; }
+        public int SeenCount { get; set; }
+
+        public DateTime TagSeenTime { get; set; }
+
+    }
+}
